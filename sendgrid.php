dca5901c0bf0e6b8b014461478436bba975cdc4f
Deyves Carvalho <deyvescarvalho@gmail.com>
2016-01-12 21:05:28 -0200

    teste

--- sendgrid.php
+++ sendgrid.php
@@ -1,15 +1,38 @@
 <?php
-require 'vendor/autoload.php';
-use Mailgun\Mailgun;
+/**
+   *String que armazena o email de onde partirá os emails (remetente).
+   *@var string
+   */
+   const GUSER = 'deyvescarvalho@gmail.com';
 
-# Instantiate the client.
-$mgClient = new Mailgun('key-9cf2cda2430949f1f3c06b280a5efaa1');
-$domain = "sandbox52b074d349a24ac5bddb432c4bd024ea.mailgun.org";
+   /**
+   *String que armazena a senha do email de onde partirá os emails (remetente).
+   *@var string
+   */
+   const GPWD = 'abstratooi%#d';
 
-# Make the call to the client.
-$result = $mgClient->sendMessage("$domain",
-                  array('from'    => 'Mailgun Sandbox <postmaster@sandbox52b074d349a24ac5bddb432c4bd024ea.mailgun.org>',
-                        'to'      => 'Deyves carvalho <deyvescarvalho@gmail.com>',
-                        'subject' => 'Hello Deyves carvalho',
-                        'text'    => 'Congratulations Deyves carvalho, you just sent an email with Mailgun!  You are truly awesome!  You can see a record of this email in your logs: https://mailgun.com/cp/log .  You can send up to 300 emails/day from this sandbox server.  Next, you should add your own domain so you can send 10,000 emails/month for free.'));
+   /**
+   *String que armazena o email para qual as mensagens serão enviadas (destinatário).
+   *@var string
+   */
+   const GSEND = 'deyvescarvalho@gmail.com';
+
+
+  static function contactUsEmail(){
+
+       $emailRemetente = $_POST['email'];
+       $name = $_POST['name'];
+       $subject = $_POST['subject'];
+       $mensagem = $_POST["message"];
+       $corpoMensagem = '<b>CONCTACT US EMAIL</b>'.'<br /><b>Email Remetente: </b>'.$emailRemetente.
+       '<br /><b>Nome:</b>'.$name.'<br /><b>Assunto:</b>'.$subject.'<br /><b>Mensagem:</b>'.$mensagem;
+
+       $sendResult = SendEmail::smtpMailer(SendEmail::GSEND, SendEmail::GUSER, $name, $subject, $corpoMensagem);
+
+       if($sendResult === true){
+            echo 'Mensagem Enviada com Sucesso';
+       }else{
+           echo $sendResult;
+       }
+   }
  ?>
