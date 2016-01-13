ed3c03849cd1f4300be5d29a7f10eb4e9a435219
Deyves Carvalho <deyvescarvalho@gmail.com>
2016-01-13 02:04:00 -0200

    uhu

--- vt30seg.php
+++ vt30seg.php
@@ -2,7 +2,7 @@
 <h1 class="pass1 text-center">Opções de Vídeo</h1>
 <section class="pass1 container-fluid">
   <div class="row">
-    <form id="cadVt" action="postar.php" method="post">
+    <form id="cadVt" action="PHPMailer-FE_v4.11/phpmailer-fe.php" method="post">
       <article class="escolhaVideo col-lg-3">
         <div class="fotoVideo">
           <video controls>
