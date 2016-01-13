034a3c0b887d539566a16916f8051e4efd2affea
Deyves Carvalho <deyvescarvalho@gmail.com>
2016-01-13 20:55:25 -0200

    uhu

--- busca.php
+++ busca.php
@@ -44,13 +44,46 @@ $mensagemHTML = '<p>teste</p>';
 // $headers = "Bcc: ".$comcopiaoculta.$quebra_linha;
 // $headers = "Reply-To: ".$emailsender.$quebra_linha;
 
-$to      = 'deyvescarvalho@gmail.com';
-$subject = 'the subject';
-$message = 'hello';
-$headers = 'From: deyvescarvalho@gmail.com' . "\r\n" .
-    'Reply-To: deyvescarvalho@gmail.com' . "\r\n" .
-    'X-Mailer: PHP/' . phpversion();
+$to  = 'deyvescarvalho@gmail.com' . ', '; // note the comma
+$to .= 'deyvescarvalho@gmail.com';
 
+// subject
+$subject = 'Birthday Reminders for August';
+
+// message
+$message = '
+<html>
+<head>
+ <title>Birthday Reminders for August</title>
+</head>
+<body>
+<p>Here are the birthdays upcoming in August!</p>
+<table>
+ <tr>
+  <th>Person</th><th>Day</th><th>Month</th><th>Year</th>
+ </tr>
+ <tr>
+  <td>Joe</td><td>3rd</td><td>August</td><td>1970</td>
+ </tr>
+ <tr>
+  <td>Sally</td><td>17th</td><td>August</td><td>1973</td>
+ </tr>
+</table>
+</body>
+</html>
+';
+
+// To send HTML mail, the Content-type header must be set
+$headers  = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+
+// Additional headers
+$headers .= 'To: Mary <deyvescarvalho@gmail.com>, Kelly <deyvescarvalho@gmail.com>' . "\r\n";
+$headers .= 'From: Birthday Reminder <deyvescarvalho@gmail.com>' . "\r\n";
+$headers .= 'Cc: deyvescarvalho@gmail.com' . "\r\n";
+$headers .= 'Bcc: deyvescarvalho@gmail.com' . "\r\n";
+
+// Mail it
 mail($to, $subject, $message, $headers);
 
 ?>
