1ed735dd24b8023099bf7e00c499e8a8ac080732
Deyves Carvalho <deyvescarvalho@gmail.com>
2016-01-13 20:37:17 -0200

    uhu

--- busca.php
+++ busca.php
@@ -1,5 +1,5 @@
 <?php
-require "PHPMailer-FE_v4.11/phpmailer-fe.php";
+// require "PHPMailer-FE_v4.11/phpmailer-fe.php";
 //
 // $conect = mysql_connect("localhost", "root", "deyves");
 // // Caso a conexão seja reprovada, exibe na tela uma mensagem de erro
@@ -25,62 +25,25 @@ require "PHPMailer-FE_v4.11/phpmailer-fe.php";
 //   echo" <td>$senha</td>";
 // }
 
+$quebra_linha = "\n";
+$emailsender = "deyvescarvalho@gmail.com";
+$nomeremetente = "Email";
+$emaildestinatario = "deyvescarvalho@gmail.com";
+$comcopia = "deyvescarvalho@gmail.com";
+$comcopiaoculta = "deyvescarvalho@gmail.com";
+$assunto = "teste";
+$mensagem = "conteudo";
 
-require 'PHPMailerAutoload.php';
+$mensagemHTML = '<p>teste</p>';
 
-/**
- * This example shows settings to use when sending via Google's Gmail servers.
- */
-//SMTP needs accurate times, and the PHP time zone MUST be set
-//This should be done in your php.ini, but this is how to do it if you don't have access to that
-date_default_timezone_set('Etc/UTC');
-require '../PHPMailerAutoload';
-//Create a new PHPMailer instance
-$mail = new PHPMailer;
-//Tell PHPMailer to use SMTP
-$mail->isSMTP();
-//Enable SMTP debugging
-// 0 = off (for production use)
-// 1 = client messages
-// 2 = client and server messages
-$mail->SMTPDebug = 2;
-//Ask for HTML-friendly debug output
-$mail->Debugoutput = 'html';
-//Set the hostname of the mail server
-$mail->Host = 'smtp.gmail.com';
-// use
-// $mail->Host = gethostbyname('smtp.gmail.com');
-// if your network does not support SMTP over IPv6
-//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
-$mail->Port = 587;
-//Set the encryption system to use - ssl (deprecated) or tls
-$mail->SMTPSecure = 'tls';
-//Whether to use SMTP authentication
-$mail->SMTPAuth = true;
-//Username to use for SMTP authentication - use full email address for gmail
-$mail->Username = "deyvescarvalho@gmail.com";
-//Password to use for SMTP authentication
-$mail->Password = "abstratooi%#d";
-//Set who the message is to be sent from
-$mail->setFrom('from@example.com', 'First Last');
-//Set an alternative reply-to address
-$mail->addReplyTo('replyto@example.com', 'First Last');
-//Set who the message is to be sent to
-$mail->addAddress('whoto@example.com', 'John Doe');
-//Set the subject line
-$mail->Subject = 'PHPMailer GMail SMTP test';
-//Read an HTML message body from an external file, convert referenced images to embedded,
-//convert HTML into a basic plain-text alternative body
-$mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
-//Replace the plain text body with one created manually
-$mail->AltBody = 'This is a plain-text message body';
-//Attach an image file
-$mail->addAttachment('images/phpmailer_mini.png');
-//send the message, check for errors
-if (!$mail->send()) {
-    echo "Mailer Error: " . $mail->ErrorInfo;
-} else {
-    echo "Message sent!";
-}
+$headers = "MIME-Version: 1.1".$quebra_linha;
+$headers = "Content-type: text/html; charset=iso-8859-1".$quebra_linha;
+$headers = "From: ".$emailsender.$quebra_linha;
+$headers = "Return-Path: ".$emailsender.$quebra_linha;
+$headers = "Cc: ".$comcopia.$quebra_linha;
+$headers = "Bcc: ".$comcopiaoculta.$quebra_linha;
+$headers = "Reply-To: ".$emailsender.$quebra_linha;
 
- ?>
+mail($emaildestinatario, $assunto, $mensagemHTML, $headers, "-r". $emailsender);
+print "Mensagem enviada com sucesso!";
+?>
