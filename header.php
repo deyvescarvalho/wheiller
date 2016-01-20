51715cfb1d9325bd83913c8036f09b73fa72a8ab
Deyves Carvalho <deyvescarvalho@gmail.com>
2016-01-20 01:38:17 -0200

    ASD

--- header.php
+++ header.php
@@ -4,6 +4,6 @@
     <meta charset="UTF-8">
     <title></title>
     <link rel="stylesheet" href="css/css/bootstrap.css">
-    <script src="//code.jquery.com/jquery-1.12.0.min.js"></script>
+    <!-- <script src="//code.jquery.com/jquery-1.12.0.min.js"></script> -->
 </head>
 <body>
