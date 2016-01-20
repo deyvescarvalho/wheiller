37806a56251847f1438939f94789dfa04243c2d0
Deyves Carvalho <deyvescarvalho@gmail.com>
2016-01-20 01:04:16 -0200

    s

--- busca.php
+++ busca.php
@@ -1,6 +1,7 @@
 <?php
 
 require 'vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
+$mail = new PHPMailer;
 
 $texto1 = $_POST['texto1'];
 $texto2 = $_POST['texto2'];
@@ -9,12 +10,9 @@ $caminho1 = $_FILES['caminho1'];
 $caminho2 = $_FILES['caminho2'];
 $caminho3 = $_FILES['caminho3'];
 
-$mail->addAttachment($caminho1["tmp_name"], $caminho1["name"]);
-$mail->addAttachment($caminho2["tmp_name"], $caminho2["name"]);
-$mail->addAttachment($caminho3["tmp_name"], $caminho3["name"]);
 
 
-$mail = new PHPMailer;
+
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'email-smtp.us-west-2.amazonaws.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
@@ -32,15 +30,17 @@ $mail->isHTML(true);                                  // Set email format to HTM
 $mail->Subject = 'PRODUÇÃO DE VT'.$id;
 
 // $mail->Body   .=  '<p>slogan da Empresa :</p>'.$sloganEmpresa.$quebralina;
-$mail->Body   .=  '<p>Texo1 :</p>'.$texto1;
+$mail->Body   =  '<p>Texo1 :</p>'.$texto1;
 $mail->Body   .=  '<p>Texo2 :</p>'.$texto2;
 $mail->Body   .=  '<p>Texo3 :</p>'.$texto3;
-$mail->Body   .=  '<p>Caminho1 :</p>'.$caminho1;
-$mail->Body   .=  '<p>Caminho2 :</p>'.$caminho2;
-$mail->Body   .=  '<p>Caminho3 :</p>'.$caminho3;
+// $mail->Body   .=  '<p>Caminho1 :</p>'.$caminho1;
+// $mail->Body   .=  '<p>Caminho2 :</p>'.$caminho2;
+// $mail->Body   .=  '<p>Caminho3 :</p>'.$caminho3;
 
 $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
+$mail->addAttachment($caminho1["tmp_name"], $caminho1["name"]);
+$mail->addAttachment($caminho2["tmp_name"], $caminho2["name"]);
+$mail->addAttachment($caminho3["tmp_name"], $caminho3["name"]);
 if(!$mail->send()) {
 	echo 'Ocorreu uma falha, e não pode ser enviado.';
 } else {
