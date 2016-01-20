e7e0380bfcf40eb7ce712838e928793a62a69c9e
Deyves Carvalho <deyvescarvalho@gmail.com>
2016-01-20 00:23:25 -0200

    mundancas

--- busca.php
+++ busca.php
@@ -1,26 +1,20 @@
 <?php
 
 require 'vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
-for ($i=0; $i < ; $i++) {
-$texto.$i = isset($_POST['texto'.$i]);
-$caminho.$i = isset($_POST['caminho'.$i]);
-}
-$texto
 
-$quebralina = '\n';
+$texto1 = $_POST['texto1'];
+$texto2 = $_POST['texto2'];
+$texto3 = $_POST['texto3'];
+$caminho1 = $_FILES['caminho1'];
+$caminho2 = $_FILES['caminho2'];
+$caminho3 = $_FILES['caminho3'];
 
-$nomeDoProduto                  = $_POST['nomeDoProduto'];
-$caracteristicaDoProduto        = $_POST['caracteristicaDoProduto'];
-$valorAvista                    = $_POST['valorAvista'];
-$produto1 = $_FILES['caminho'];
-$anexo2 = $_FILES['caminho'];
-
+$mail->addAttachment($caminho1["tmp_name"], $caminho1["name"]);
+$mail->addAttachment($caminho2["tmp_name"], $caminho2["name"]);
+$mail->addAttachment($caminho3["tmp_name"], $caminho3["name"]);
 
 
 $mail = new PHPMailer;
-
-
-
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'email-smtp.us-west-2.amazonaws.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
@@ -37,21 +31,19 @@ $mail->isHTML(true);                                  // Set email format to HTM
 
 $mail->Subject = 'PRODUÇÃO DE VT'.$id;
 
-$mail->Body   .=  '<p>slogan da Empresa :</p>'.$sloganEmpresa.$quebralina;
-$mail->Body   .=  '<p>Logomarca :</p>'.$logoEmpresa.$quebralina;
-
+// $mail->Body   .=  '<p>slogan da Empresa :</p>'.$sloganEmpresa.$quebralina;
+$mail->Body   .=  '<p>Texo1 :</p>'.$texto1;
+$mail->Body   .=  '<p>Texo2 :</p>'.$texto2;
+$mail->Body   .=  '<p>Texo3 :</p>'.$texto3;
+$mail->Body   .=  '<p>Caminho1 :</p>'.$caminho1;
+$mail->Body   .=  '<p>Caminho2 :</p>'.$caminho2;
+$mail->Body   .=  '<p>Caminho3 :</p>'.$caminho3;
 
 $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
 if(!$mail->send()) {
-  echo 'Ocorreu uma falha, e não pode ser enviado.';
+	echo 'Ocorreu uma falha, e não pode ser enviado.';
 } else {
-  echo 'Pedido enviado com sucesso!';
+	echo 'Pedido enviado com sucesso!';
 }
-
-
-
-
-
-
 ?>
