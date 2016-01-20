b80ab872e770843338dbbb6590f1235b4fcf2d00
Deyves Carvalho <deyvescarvalho@gmail.com>
2016-01-20 19:39:16 -0200

    mudanças ok

--- busca.php
+++ busca.php
@@ -4,11 +4,13 @@ require 'vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
 $mail = new PHPMailer;
 
 $texto1 = $_POST['texto1'];
-// $texto2 = $_POST['texto2'];
-// $texto3 = $_POST['texto3'];
-$caminho1 = $_FILES["caminho1"];
-// $caminho2 = $_FILES["caminho2"];
-// $caminho3 = $_FILES['caminho3'];
+$texto2 = $_POST['texto2'];
+$texto3 = $_POST['texto3'];
+
+$anexo = $_FILES["anexo1"];
+$anexo = $_FILES["anexo2"];
+$anexo = $_FILES["anexo3"];
+
 
 
 
@@ -16,29 +18,28 @@ $caminho1 = $_FILES["caminho1"];
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 $mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'deyvescarvalho@gmail.com';                 // SMTP username
-$mail->Password = 'abstratooi%#dka';                           // SMTP password
+$mail->Username = 'wheillercarvalho@gmail.com';                 // SMTP username
+$mail->Password = 'coloquesuasenhaaqui';                           // SMTP password
 $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
 $mail->Port = 587;                                    // TCP port to connect to
 // $mail->setLanguage('br');
 
-$mail->setFrom('deyvescarvalho@gmail.com');
-$mail->addAddress('deyvescarvalho@gmail.com');     // Add a recipient
-$mail->addAddress('ellen@example.com');               // Name is optional
-// $mail->addReplyTo('info@example.com', 'Information');
-$mail->addCC('cc@example.com');
-$mail->addBCC('bcc@example.com');
-$mail->addReplyTo('deyvescarvalho@gmail.com');
+$mail->setFrom('wheillercarvalho@gmail.com');
+$mail->addAddress('wheillercarvalho@gmail.com');     // Add a recipient
 
 
-$mail->addAttachment($caminho1["tmp_name"], $caminho1["name"]);
+$mail->addAttachment($anexo1["tmp_name"], $anexo1["name"]);
+$mail->addAttachment($anexo2["tmp_name"], $anexo2["name"]);
+$mail->addAttachment($anexo3["tmp_name"], $anexo3["name"]);
 
 $mail->isHTML(true);                                  // Set email format to HTML
 
 
 $mail->Subject = 'PRODUÇÃO DE VT';
 
-$mail->Body   =  '<p>Texo1 :</p>'.$texto1;
+$mail->Body    =  '<p>Texo1 :</p>'.$texto1.'\n';
+$mail->Body   .=  '<p>Texo2 :</p>'.$texto2.'\n';
+$mail->Body   .=  '<p>Texo3 :</p>'.$texto3.'\n';
 $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
 
--- index.php
+++ index.php
@@ -1,21 +1,23 @@
 <?php require_once 'header.php';  ?>
       <form id="cadVt" name="formvalendo" method="POST" action="busca.php"  >
-        <?php
-        $quantidadeFormularios = 3;
-
-        for ($i=0; $i < $quantidadeFormularios; $i++) {
-        ?>
           <div class="form-group">
-            <label for="opcINput">TEXTO <?php echo $i +1;  ?></label>
-            <input type="text" name="texto<?php echo $i +1;  ?>" id="opcINput" class="txtAbertura form-control" placeholder="Inserir texto de abertura">
-            <input type="file" name="caminho<?php echo $i +1;  ?>" class="form-control" id="arquivo" accept="image/*">
-
+            <label for="opcINput">TEXTO 1</label>
+            <input type="text" name="texto1" id="opcINput" class="txtAbertura form-control" placeholder="Inserir texto de abertura">
+            <input type="file" name="anexo1" class="form-control" id="arquivo" accept="image/*">
           </div>
-
           <br>
-      <?php
-        }
-      ?>
+          <div class="form-group">
+            <label for="opcINput">TEXTO 2</label>
+            <input type="text" name="texto2" id="opcINput" class="txtAbertura form-control" placeholder="Inserir texto de abertura">
+            <input type="file" name="anexo2" class="form-control" id="arquivo" accept="image/*">
+          </div>
+          <br>
+          <div class="form-group">
+            <label for="opcINput">TEXTO 3</label>
+            <input type="text" name="texto3" id="opcINput" class="txtAbertura form-control" placeholder="Inserir texto de abertura">
+            <input type="file" name="anexo3" class="form-control" id="arquivo" accept="image/*">
+          </div>
+          <br>
         <input type="submit" id="salvar" name="button" class="btn btn-default"  value="Finalizar">
       </form>
 <?php require_once 'footer.php';?>
