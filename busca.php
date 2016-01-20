ca7c7aab8c18766ffb45c2849e068fcb57122c8f
Deyves Carvalho <deyvescarvalho@gmail.com>
2016-01-20 01:23:02 -0200

    AAA

--- busca.php
+++ busca.php
@@ -1,17 +1,17 @@
 <?php
 
 require 'vendor/phpmailer/phpmailer/PHPMailerAutoload.php';
+$mail = new PHPMailer;
 
-$texto = $_POST['texto'];
+$texto1 = $_POST['texto1'];
 $texto2 = $_POST['texto2'];
 $texto3 = $_POST['texto3'];
-$caminho = $_FILES["caminho"];
+$caminho1 = $_FILES["caminho1"];
 $caminho2 = $_FILES["caminho2"];
 $caminho3 = $_FILES['caminho3'];
 
 
 
-$mail = new PHPMailer;
 
 $mail->isSMTP();                                      // Set mailer to use SMTP
 $mail->Host = 'email-smtp.us-west-2.amazonaws.com';  // Specify main and backup SMTP servers
@@ -29,23 +29,17 @@ $mail->isHTML(true);                                  // Set email format to HTM
 
 $mail->Subject = 'PRODUÇÃO DE VT';
 
-// $mail->Body   .=  '<p>slogan da Empresa :</p>'.$sloganEmpresa.$quebralina;
-$mail->Body   =  '<p>Texo1 :</p>'.$texto;
-// $mail->Body   .=  '<p>Texo2 :</p>'.$texto2;
-// $mail->Body   .=  '<p>Texo3 :</p>'.$texto3;
-// $mail->Body   .=  '<p>Caminho1 :</p>'.$caminho1;
-// $mail->Body   .=  '<p>Caminho2 :</p>'.$caminho2;
-// $mail->Body   .=  '<p>Caminho3 :</p>'.$caminho3;
+$mail->Body   =  '<p>Texo1 :</p>'.$texto1;
+$mail->Body   .=  '<p>Texo2 :</p>'.$texto2;
+$mail->Body   .=  '<p>Texo3 :</p>'.$texto3;
 
-// $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
 
-if($caminho["size"] > 0)
-	{
-		$mail->addAttachment($caminho["tmp_name"], $caminho["name"]);
-	}
-// $mail->addAttachment($caminho2["tmp_name"], $caminho2["name"]);
-// $mail->addAttachment($caminho3["tmp_name"], $caminho3["name"]);
+
+		$mail->addAttachment($caminho1["tmp_name"], $caminho1["name"]);
+$mail->addAttachment($caminho2["tmp_name"], $caminho2["name"]);
+$mail->addAttachment($caminho3["tmp_name"], $caminho3["name"]);
 if(!$mail->send()) {
 	echo 'Ocorreu uma falha, e não pode ser enviado.';
 } else {
--- index.php
+++ index.php
@@ -1,13 +1,19 @@
 <?php require_once 'header.php';  ?>
       <form id="cadVt" name="formvalendo" method="POST" action="busca.php"  >
+        <?php
+        $quantidadeFormularios = 3;
 
+        for ($i=0; $i < $quantidadeFormularios; $i++) {
+        ?>
           <div class="form-group">
-            <label for="opcINput">TEXTO </label>
-            <input type="text" name="texto1" id="opcINput" class="txtAbertura form-control" placeholder="Inserir texto de abertura">
-            <input type="file" name="caminho" class="form-control" id="arquivo" accept="image/*">
+            <label for="opcINput">TEXTO <?php echo $i +1;  ?></label>
+            <input type="text" name="texto<?php echo $i +1;  ?>" id="opcINput" class="txtAbertura form-control" placeholder="Inserir texto de abertura">
+            <input type="file" name="caminho<?php echo $i +1;  ?>" class="form-control" id="arquivo" accept="image/*">
           </div>
           <br>
-
+      <?php
+        }
+      ?>
         <input type="submit" id="salvar" name="button" class="btn btn-default"  value="Finalizar">
       </form>
 <?php require_once 'footer.php';?>
